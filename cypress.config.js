f7b15b49f8dc45ea0725537693d4a509cb73d64f
Stefan Pavlovic <pavlovicstefan135@gmail.com>
2023-05-12 00:28:11 +0200

    scrip

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   defaultCommandTimeout:6000,
-  projectId: 'aapsxnp9',
+  projectId: 'bft6k7',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/intergration/test/Test2.js
+++ cypress/intergration/test/Test2.js
@@ -57,6 +57,7 @@ describe('test2',() => {
         cy.get('div.mouse-hover-content').invoke('show');
         cy.contains('Top').click();
         cy.url().should('include','top');
+        
     } )
 
 
